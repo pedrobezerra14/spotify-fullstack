52766d4c943c8840e2a0401a60f8cbb8f67a8633
Pedro Henrique <pedrohrezendeb07@gmail.com>
2025-05-12 13:03:10 -0300

    criando base do app antes de montar estrutura de páginas

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,26 +1,30 @@
 import React from 'react';
 import SingleItem from './SingleItem';
 
-const ItemList = ({ title, items }) => { 
+const ItemList = ({ title, items, itemsArray, path, idPath}) => { 
     return (
         <div className="item-list">
             <div className="item-list__header">
                 <h2> { title } Populares</h2>
-                <a className="item-list__link" href="/">
+                <a href={path} className="item-list__link" >
                 Mostrar tudo
                 </a>
             </div>
                 
             <div className="item-list__container">
-                <SingleItem />
-                <SingleItem />
-                <SingleItem />
-                <SingleItem />
-                <SingleItem />
-
+                {itemsArray
+                    .filter((currentValue, index) => index < items)
+                    .map((currObj, index) => (
+                    <
+                        SingleItem 
+                        idPath={idPath}
+                        {...currObj}
+                        key={`${title}-${index}`} 
+                    />
+                ))}
             </div>
         </div>
     );
-}
+};
 
-export default ItemList
+export default ItemList;
--- src/components/SingleItem.jsx
+++ src/components/SingleItem.jsx
@@ -2,22 +2,28 @@ import React from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCirclePlay } from '@fortawesome/free-solid-svg-icons';
 
-function SingleItem() {
+const SingleItem = ({ id, name, image, banner, artist, idPath}) => {
   return (
-    <div className="single-item">
+    <a href={`${idPath}/${id}`} className="single-item">
         <div className="single-item__div-image-button">
             <div className="single-item__div-image">
-                <img className="single-item__image" src="https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4" alt="Imagem do artista X" />
+                <img 
+                    className="single-item__image" 
+                    src={image} 
+                    alt={`Imagem do artista ${name}`} 
+                />
             </div>
                 <FontAwesomeIcon className="single-item__icon" icon={faCirclePlay} />
         </div>
         <div className="single-item__texts">
             <div className="single-item__2lines">
-                <p className="single-item__title">Henrique & Juliano</p>
+                <p className="single-item__title">{name}</p>
             </div>
-                <p className="single-item__type">Artista</p>
+                <p className="single-item__type">
+                    {artist ?? "Artista"}
+                </p>
         </div>
-    </div>
+    </a>
   )
 }
 
